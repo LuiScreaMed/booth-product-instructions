6a6c83847f1c5fb1f8eb8cf0e3f0a3c3cfbe3b18
LuiScreaMed <lui5@qq.com>
2025-02-14 22:56:38 +0800

    调整文档图片路径切片索引

--- src/plugins/remark-image-parse.js
+++ src/plugins/remark-image-parse.js
@@ -6,8 +6,8 @@ export default function () {
         const docid = pathArr[pathArr.length - 2];
 
         visit(ast, "image", (node, index, parent) => {
-            if (node.url && (node.url.startsWith("./assets") || node.url.startsWith("./Assets"))) {
-                node.url = `pathname:///instruction-assets/${docid}/${node.url.slice(8)}`;
+            if (node.url && (node.url.startsWith("./assets/") || node.url.startsWith("./Assets/"))) {
+                node.url = `pathname:///instruction-assets/${docid}/${node.url.slice(9)}`;
             }
         });
     };
